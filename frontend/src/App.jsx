3c214b34d9cde61642a18738b1189ae76eda299d
Vishahl99 <vishalkumar.singh.cer21@itbhu.ac.in>
2025-07-03 11:56:11 +0530

    Project Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <h1 className='text-4xl'>Real-time Collaboration Platform </h1>
+    </>
+  )
+}
+
+export default App
